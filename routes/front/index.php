645514b43089fa4e98bc535dc47061467acccd9c
Chen-Yen-Xun <norush5566@gmail.com>
2024-09-08 23:18:39 +0800

    Nav Bar & Slider初稿完成

--- routes/front/index.php
+++ routes/front/index.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Front\IndexController;
 use App\Http\Controllers\Front\LogoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get("/", [LogoController::class, "logo"]);
+Route::get("/index", [IndexController::class, "index"]);
