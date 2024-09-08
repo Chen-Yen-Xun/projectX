d4cb6a4a876be07195f7aebeb3aaa3deec552025
Chen-Yen-Xun <norush5566@gmail.com>
2024-09-08 12:32:14 +0800

    Logo頁面完成

---
+++ app/Http/Controllers/Front/LogoController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Front;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LogoController extends Controller
+{
+    public function logo()
+    {
+        return view("admin.logo");
+    }
+}
