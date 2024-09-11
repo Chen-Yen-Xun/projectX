b8dd69fa5d6f2fd4ec3d32eb5e88baad9553d51f
Chen-Yen-Xun <norush5566@gmail.com>
2024-09-11 15:27:17 +0800

    footer socialmedia未完成

--- resources/views/front/footer/footer.blade.php
+++ resources/views/front/footer/footer.blade.php
@@ -1,4 +1,37 @@
-
+<style>
+    .sm_icon01:hover
+    {
+        background-image: url('images/front/socialmedia/sm_yt_h.png');
+    }
+    .sm_icon02
+    {
+        background-image: url('images/front/socialmedia/sm_re_h.png');
+    }
+    .sm_icon03
+    {
+        background-image: url('images/front/socialmedia/sm_fb_h.png');
+    }
+    .sm_icon04
+    {
+        background-image: url('images/front/socialmedia/sm_li_h.png');
+    }
+    .sm_icon05
+    {
+        background-image: url('images/front/socialmedia/sm_ig_h.png');
+    }
+    .sm_icon06
+    {
+        background-image: url('images/front/socialmedia/sm_th_h.png');
+    }
+    .sm_icon07
+    {
+        background-image: url('images/front/socialmedia/sm_x_h.png');
+    }
+    .sm_icon08
+    {
+        background-image: url('images/front/socialmedia/sm_we_h.png');
+    }
+</style>
 <div class="row py-5">
     <div class="col-md-4">
         <div class="h5 fw-700 text-color02">
@@ -12,10 +45,10 @@
     <div class="col-md-4 offset-md-4">
         <div class="row">
             <div class="col-md-2 offset-md-1">
-                <a href="#"><img src="images/front/socialmedia/sm_yt.png" alt=""></a>
+                <a href="#"><img class="sm_icon01" src="images/front/socialmedia/sm_yt.png" alt=""></a>
             </div>
             <div class="col-md-2">
-                <a href="#"><img src="images/front/socialmedia/sm_we.png" alt=""></a>
+                <a href="#"><img src="images/front/socialmedia/sm_re.png" alt=""></a>
             </div>
             <div class="col-md-2">
                 <a href="#"><img src="images/front/socialmedia/sm_fb.png" alt=""></a>
