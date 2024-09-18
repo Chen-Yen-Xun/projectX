0813f71ab368ef0dff2cc931f7218260d8e3fcb9
Chen-Yen-Xun <norush5566@gmail.com>
2024-09-18 10:32:51 +0800

    產品推薦修改

--- resources/views/front/product/recommend.blade.php
+++ resources/views/front/product/recommend.blade.php
@@ -6,64 +6,24 @@
             </div>
             <div class="row mt-5">
                 <div class="col-md-6">
-                    <div class="card mb-5">
-                        <div class="card-header product_r_card_img01 bg-cover">
-                        </div>
-                        <div class="card-body">
-                            <div class="card-text">
-                                <div class="h3 fw-900 text-pc-color-warn01">產品名稱：GX8700-L</div>
-                                <div class="h5 fw-700 text-pc-color-minor02">2024年全新設計，手動噴槍界的勞斯萊斯。</div>
-                            </div>
-                        </div>
-                        <div class="card-footer text-center">
-                            <button class="btn bg-btn05 fw-900">更多</button>
-                        </div>
+                    <video src="videos/product01.mp4" autoplay muted loop width="700px" height="400px"></video>
+                </div>
+                <div class="col-md-5 offset-md-1">
+                    <div class="w-100 h-100 justify-content-center align-content-center text-center">
+                        <div class="h3 fw-900 text-pc-color-warn01">產品名稱：大旋風噴房</div>
+                        <div class="h5 fw-700 text-pc-color-minor01 py-3">產線潔淨的秘密武器。</div>
+                        <button class="btn bg-btn05 fw-900">更多</button>
+                    </div>
+                </div>
+                <div class="col-md-5 offset-md-1">
+                    <div class="w-100 h-100 justify-content-center align-content-center text-center">
+                        <div class="h3 fw-900 text-pc-color-main03">產品名稱：GX8700-L</div>
+                        <div class="h5 fw-700 text-pc-color-minor01 py-3">2024年全新設計，手動噴槍界的勞斯萊斯。</div>
+                        <button class="btn bg-btn05 fw-900">更多</button>
                     </div>
                 </div>
                 <div class="col-md-6">
-                    <div class="card mb-5">
-                        <div class="card-header product_r_card_img02 bg-cover">
-                        </div>
-                        <div class="card-body">
-                            <div class="card-text">
-                                <div class="h3 fw-900 text-pc-color-warn01">產品名稱：RX168-L</div>
-                                <div class="h5 fw-700 text-pc-color-minor02">2024年全新設計，自動噴槍界的賓利。</div>
-                            </div>
-                        </div>
-                        <div class="card-footer text-center">
-                            <button class="btn bg-btn05 fw-900">更多</button>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="card mb-5">
-                        <div class="card-header product_r_card_img03 bg-cover">
-                        </div>
-                        <div class="card-body">
-                            <div class="card-text">
-                                <div class="h3 fw-900 text-pc-color-warn01">產品名稱：小旋風噴房</div>
-                                <div class="h5 fw-700 text-pc-color-minor02">無法忍受工作間粉塵嗎？</div>
-                            </div>
-                        </div>
-                        <div class="card-footer text-center">
-                            <button class="btn bg-btn05 fw-900">更多</button>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="card mb-5">
-                        <div class="card-header product_r_card_img04 bg-cover">
-                        </div>
-                        <div class="card-body">
-                            <div class="card-text">
-                                <div class="h3 fw-900 text-pc-color-warn01">產品名稱：大旋風噴房</div>
-                                <div class="h5 fw-700 text-pc-color-minor02">產線潔淨的秘密武器。</div>
-                            </div>
-                        </div>
-                        <div class="card-footer text-center">
-                            <button class="btn bg-btn05 fw-900">更多</button>
-                        </div>
-                    </div>
+                    <video src="videos/product02.mp4" autoplay muted loop width="600px" height="400px"></video>
                 </div>
             </div>
         </div>
